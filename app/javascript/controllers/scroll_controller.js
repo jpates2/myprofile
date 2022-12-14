95b7c7d54e975450cac79ad1fe8e742f1dba99bb
Jess Pates <jesspates@hotmail.co.uk>
2022-12-14 22:48:47 +0100

    day two updates

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="scroll"
+export default class extends Controller {
+  connect() {
+    console.log("Check")
+  }
+
+  scroll() {
+    if
+  }
+}
