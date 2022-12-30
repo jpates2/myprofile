a738b506bef6d95809d29ad531fe00f637abbe35
Jess Pates <jesspates@hotmail.co.uk>
2022-12-30 21:18:46 +0000

    JS expand added to project cards

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -4,7 +4,7 @@ import Typed from 'typed.js';
 // Connects to data-controller="typed"
 export default class extends Controller {
   connect() {
-    console.log("check")
+    // console.log("check")
     let options = {
       strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
       typeSpeed: 40
