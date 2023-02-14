020584259aa5127d638c18dfc9d37deed65a10eb
Jess Pates <jesspates@hotmail.co.uk>
2023-02-14 20:25:03 +0100

    click event added to show modal windows

--- app/javascript/controllers/typed_controller.js
+++ app/javascript/controllers/typed_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
-import Typed from 'typed.js';
+// import { Controller } from "@hotwired/stimulus"
+// import Typed from 'typed.js';
 
-// Connects to data-controller="typed"
-export default class extends Controller {
-  connect() {
-    // console.log("check")
-    let options = {
-      strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
-      typeSpeed: 40
-    };
+// // Connects to data-controller="typed"
+// export default class extends Controller {
+//   connect() {
+//     // console.log("check")
+//     let options = {
+//       strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+//       typeSpeed: 40
+//     };
 
-    let typed = new Typed('.element', options);
-  }
-}
+//     let typed = new Typed('.element', options);
+//   }
+// }
---
+++ app/javascript/script.js
@@ -0,0 +1,31 @@
+// var coll = document.getElementsByClassName("collapsible");
+// var i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     let content = this.nextElementSibling;
+//     if (content.style.maxHeight){
+//       content.style.maxHeight = null;
+//     } else {
+//       content.style.maxHeight = content.scrollHeight + "px";
+//     }
+//   });
+// }
+
+// function changeIcon(anchor) {
+//   var icon = anchor.querySelector("i");
+//   icon.classList.toggle('fa-square-plus');
+//   icon.classList.toggle('fa-square-minus');
+
+//   // anchor.querySelector("span").textContent = icon.classList.contains('fa-square-plus') ? "Read more" : "Read less";
+// }
+
+// const modal = document.querySelectorAll(".modal");
+// const btnOpenWindow = document.querySelectorAll(".show-window");
+
+// for (let i = 0; i < btnOpenWindow.length; i++) {
+//   btnOpenWindow[i].addEventListener("click", function () {
+//     window.classList.remove("hidden");
+//   })
+// };
